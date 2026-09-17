0b303f8f162f7e90c55e7c78a81f580cd202700c
Liliya Panfilova <78988408+lpanfilova@users.noreply.github.com>
2026-09-16 23:01:30 -0400

    Create ExampleTest.java

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTest {
+
+    @Test
+    void additionWorks() {
+        assertEquals(4, 2 + 2);
+    }
+}
